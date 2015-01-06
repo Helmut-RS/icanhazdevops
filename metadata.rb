f30570098c6c501afc42c6ebaa8d68b0e3aebaa7
Helmut Knoepfle <helmut.knoepfle@rackspace.com>
2015-01-06 14:24:12 -0600

    bump metadata

--- metadata.rb
+++ metadata.rb
@@ -5,5 +5,5 @@ maintainer_email 'none'
 license 'MIT'
 description 'Installs/Configures icanhazdevops'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.2.2'
+version '0.2.3'
 depends 'apache2'
